41e29336fd7f78dc4176fa2b219d99b99e3d99b1
VincentJonathan <vinjongm@gmail.com>
2021-02-25 18:36:20 +0700

    update build script

--- build.js
+++ build.js
@@ -1,16 +1,24 @@
 const { exec } = require("child_process");
+var archiver = require("archiver");
 const fs = require("fs");
 
 const package = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
+const appName = `${package.name}_${package.version}`;
+const targetFolder = `./dist/${appName}`;
 
 fs.rmdirSync("./dist", { recursive: true });
 
 const run = exec(
-  `npm run build:in -- -rn ${package.name}_${package.version} -rv ${package.version}`
+  `npm run build:in -- -rn ${appName} -rv ${package.version} -d ${targetFolder}`
 );
 run.stdout.on("data", console.log);
 
 run.on("close", () => {
-  fs.copyFileSync("./config.example", "./dist/config");
-  fs.unlinkSync("./dist/index.js");
+  fs.copyFileSync("./config.example", `${targetFolder}/config`);
+
+  const output = fs.createWriteStream(`./dist/${appName}.zip`);
+  const archive = archiver("zip");
+  archive.pipe(output);
+  archive.directory(`${targetFolder}`, appName);
+  archive.finalize();
 });
